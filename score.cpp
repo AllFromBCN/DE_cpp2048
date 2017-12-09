a6025f24c8fb2434c2efa02e5e18b0ae537951b3
13heejinkim <thguffkd@naver.com>
2017-12-09 20:00:19 +0900

    Update score.cpp

--- score.cpp
+++ score.cpp
@@ -1,5 +1,17 @@
-//
-// Created by 정수민 on 2017. 12. 9..
-//
-
 #include "score.h"
+#include <iostream>
+
+using namespace std;
+
+Score::Score() {}
+Score::~Score() {}
+
+void Score::addScore(Block b1, Block b2)
+{
+	int temp = b1.getNum + b2.getNum;
+	if (b1.getBonus == true)
+	{temp *= 2;}
+	if (b2.getBonus == true)
+	{temp *= 2;}
+	score += temp;
+}
