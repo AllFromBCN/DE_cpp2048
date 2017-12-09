cbfb98722e3ea264963ff02d4ccfb8317369807f
13heejinkim <thguffkd@naver.com>
2017-12-10 00:38:58 +0900

    Update score.h

--- score.h
+++ score.h
@@ -1,8 +1,10 @@
 /*
 기본사항 : 생성자, 소멸자, 복사생성자, this, 예외처리
 */
+#pragma once
+#ifndef SCORE_H
+#define SCORE_H
 
-#include "map.h"
 #include "block.h"
 #include <iostream>
 
@@ -17,3 +19,5 @@ public:
 	void addScore(Block, Block);
 	int getScore();
 };
+
+#endif
