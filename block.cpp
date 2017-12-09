2f9ab565df9f2adf1b9dceab355d86e31f5823d4
13heejinkim <thguffkd@naver.com>
2017-12-09 15:48:09 +0900

    Update block.cpp

--- block.cpp
+++ block.cpp
@@ -1,17 +1,24 @@
-
 #define kUp 119 // to delete
 #define kDown 115 // to delete
 #define kLeft 97 // to delete
 #define kRight 100 // to delete
 
 #include <iostream>
-#include <block.h>
+#include "block.h"
 using namespace std;
 
-Block::Block(int row, int col, int num){
-  this->row = row;
-  this->col = col;
-  this->num = num;
+Block::Block()
+{
+	this->row = -1;
+	this->col = -1;
+	this->num = 0;
+	this->bonus = false;
+}
+Block::Block(int row, int col, int num, bool bonus) {
+	this->row = row;
+	this->col = col;
+	this->num = num;
+	this->bonus = bonus;
 };
 
 /*
