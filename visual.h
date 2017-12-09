e400dd9411408cc991ebad3d8534f3f6e251571d
13heejinkim <thguffkd@naver.com>
2017-12-09 22:11:18 +0900

    Update visual.h

--- visual.h
+++ visual.h
@@ -1,31 +1,24 @@
-#ifndef VISUAL_H
-#define VISUAL_H
-
-//
-// Created by 정수민 on 2017. 12. 9..
-//
 /*
 기본사항 : 생성자, 소멸자, 복사생성자, this, 예외처리
 const 함수 : map print 시
 */
-#include "map.h"
+
+#ifndef VISUAL_H
+#define VISUAL_H
+
 #include "score.h"
+#include "map.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 class Visual {
-	int size;
-	Map map;
-
 public:
 	Visual() {}
 	~Visual() {}
 
-	void show();
-	void start();
-	void setMap(int);
+	void show(Map);
 	
 };
 #endif //VISUAL_H
