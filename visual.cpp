1a6635b995507d98313380fce95e0960ffe03351
13heejinkim <thguffkd@naver.com>
2017-12-09 20:40:44 +0900

    Update visual.cpp

--- visual.cpp
+++ visual.cpp
@@ -27,30 +27,28 @@ void Visual::show()
 			}
 			temp[4] = ' ';
 
-			/*
-			int num = map[i][j]; //@가 없는 map만 실행됨.. @을 제거하는 방법을 찾아야함 todo
+			int num = map.map[i][j].getNum();
 			int k=3;
 			while(1)
 			{
-			if(num!=0)
-			{
-			temp[k--] = num%10;
-			num = num/10;
+				if(num!=0)
+				{
+					temp[k--] = num%10;
+					num = num/10;
+				}
+				else break;
 			}
-			else break;
-			}
-			if(map[i][j].find('@')) // 왜 찾아지지? 그래서 차라리 map을 int로만 만들고, bonus인지를 따로 체크하는 array만드는것은?
+			if(map.map[i][j].getBonus())
 			{
-			temp[k] = '@';
-			}*/
-
+				temp[k] = '@';
+			}
 			cout << temp;
 		}
 		cout << endl;
 	}
 };
 
-Map Visual::start()
+void Visual::start()
 {
 	string enter;
 	cout << "You make the 2048 tile!" << endl;
