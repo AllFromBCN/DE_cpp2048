79acc9cd3d4717e9b47938bde44ee4e6e5454d15
13heejinkim <thguffkd@naver.com>
2017-12-17 21:28:30 +0900

    Update block.cpp

--- block.cpp
+++ block.cpp
@@ -1,8 +1,3 @@
-#define kDown 115 // to delete
-#define kLeft 97 // to delete
-#define kRight 100 // to delete
-
-#include <iostream>
 #include "block.h"
 
 using namespace std;
@@ -16,12 +11,16 @@ Block::Block(int num, bool bonus){
 
 Block::~Block(){}
 
-int Block::getNum()
-{ return num; }
-bool Block::getBonus()
-{ return bonus;};
+int Block::getNum(){
+	return num;
+}
+bool Block::getBonus(){
+	return bonus;
+}
 
-void Block::setNum(int num)
-{ this->num = num; }
-void Block::setBonus(bool bonus)
-{ this->bonus = bonus; }
+void Block::setNum(int num){
+	this->num = num;
+}
+void Block::setBonus(bool bonus){
+	this->bonus = bonus;
+}
