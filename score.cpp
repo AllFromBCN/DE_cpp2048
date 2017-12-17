6c0c700c8c9fc9d013747f1b99dd45bdb1f9bd64
13heejinkim <thguffkd@naver.com>
2017-12-17 21:29:14 +0900

    Update score.cpp

--- score.cpp
+++ score.cpp
@@ -1,6 +1,4 @@
 #include "score.h"
-#include "block.h"
-#include <iostream>
 
 using namespace std;
 
@@ -11,9 +9,9 @@ void Score::addScore(Block b1, Block b2)
 {
 	int temp = b1.getNum() + b2.getNum();
 	if (b1.getBonus() == true)
-		{temp *= 2;}
+		temp *= 2;
 	if (b2.getBonus() == true)
-		{temp *= 2;}
+		temp *= 2;
 	score += temp;
 }
 
