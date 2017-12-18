3f5d2142ebcd9b3e7ea1e9e473f7857b199f1d89
Soo <soominjeong93@gmail.com>
2017-12-18 21:48:51 +0900

    Update main.cpp
    updated string input

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 	Map ori_map;
 	int i;
 	cout << "You make the 2048 tile!" << endl;
-	cout << "You can use the A W D S button" << endl;
+	cout << "You can use the a w d s button" << endl;
 	cout << "If you want to start with 4x4 map, then enter the 's'" << endl;
 	cout << "If you want to start with a map larger then 4X4, then enter the number between 5 and 9" << endl;
 	while (1)
@@ -37,20 +37,46 @@ int main()
 			cout << "Out of range. Please re-enter" << endl;
 		}
 	}
-	char key;
+    string key;
 	while (1)
 	{
 		v.show(ori_map);
 		cout << endl;
+        if (ori_map.countEmpty() == 0 && ori_map.check() == 0)
+        {
+            cout << "Game Over!" << endl;
+            return 0;
+        }
+
 		cout << "Enter the direction key" << endl;
         cin >> key;
-		ori_map.move(v.input(key));//입력키 받는부분
 
-		if (ori_map.countEmpty() == 0 && ori_map.check() == false)
-		{
-			cout << "Game Over!" << endl;
-			return 0;
-		}
+        while(key != "a" && key != "s" && key != "w" && key != "d")
+        {
+            cout << "Please enter another direction key" << endl;
+            cin >> key;
+            if (key == "a" || key == "s" || key == "w" || key == "d")
+                break;
+        }
+
+        if(!ori_map.countEmpty())
+        {
+            if(ori_map.check()==2 && (key == "w" || key == "s"))
+            {
+                cout << "Game Over!" << endl;
+                return 0;
+            }
+            else if(ori_map.check()==3 && (key == "a" || key == "d"))
+            {
+                cout << "Game Over!" << endl;
+                return 0;
+            }
+        }
+
+		ori_map.move(v.input(key));//입력키 받는부분
+        ori_map.operator+();
+
+
+
 	}
-	return 0;
 }
