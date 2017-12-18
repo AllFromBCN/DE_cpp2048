848820e73663dea38ec0cc5edc9b1b954647278f
13heejinkim <thguffkd@naver.com>
2017-12-18 21:57:29 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "visual.h"
+#include <string>
+
 using namespace std;
 
 int main()
@@ -8,7 +10,7 @@ int main()
 	Map ori_map;
 	int i;
 	cout << "You make the 2048 tile!" << endl;
-	cout << "You can use the a w d s button" << endl;
+	cout << "You can use the A W D S button" << endl;
 	cout << "If you want to start with 4x4 map, then enter the 's'" << endl;
 	cout << "If you want to start with a map larger then 4X4, then enter the number between 5 and 9" << endl;
 	while (1)
@@ -37,46 +39,46 @@ int main()
 			cout << "Out of range. Please re-enter" << endl;
 		}
 	}
-    string key;
+	string key;
 	while (1)
 	{
 		v.show(ori_map);
 		cout << endl;
-        if (ori_map.countEmpty() == 0 && ori_map.check() == 0)
-        {
-            cout << "Game Over!" << endl;
-            return 0;
-        }
+		
+		if (ori_map.countEmpty() == 0 && ori_map.check() == 0)
+		{
+			cout << "Game Over!" << endl;
+			//cin >> key;
+			return 0;
+		}
 
 		cout << "Enter the direction key" << endl;
-        cin >> key;
+		cin >> key;
 
-        while(key != "a" && key != "s" && key != "w" && key != "d")
-        {
-            cout << "Please enter another direction key" << endl;
-            cin >> key;
-            if (key == "a" || key == "s" || key == "w" || key == "d")
-                break;
-        }
+		while (key != "a" && key != "s" && key != "w" && key != "d")
+		{
+			cout << "You have inserted something other than 'a','s','d',w'" << endl;
+			cout << "Re-Enter the direction key" << endl;
+			cin >> key;
+			if (key == "a" || key == "s" || key == "w" || key == "d")
+				break;
+		}
 
-        if(!ori_map.countEmpty())
-        {
-            if(ori_map.check()==2 && (key == "w" || key == "s"))
-            {
-                cout << "Game Over!" << endl;
-                return 0;
-            }
-            else if(ori_map.check()==3 && (key == "a" || key == "d"))
-            {
-                cout << "Game Over!" << endl;
-                return 0;
-            }
-        }
-
-		ori_map.move(v.input(key));//입력키 받는부분
-        ori_map.operator+();
-
-
-
+		if (ori_map.countEmpty() == 0)
+		{
+			if (ori_map.check() == 2 && (key == "w" || key == "s"))
+			{
+				cout << "Not move! You Re-Enter the direction key" << endl;
+				continue;
+			}
+			else if (ori_map.check() == 3 && (key == "a" || key == "d"))
+			{
+				cout << "Not move! You Re-Enter the direction key" << endl;
+				continue;
+			}
+		}
+		ori_map.move(v.input(key));
+		ori_map.operator+();
 	}
+	return 0;
 }
